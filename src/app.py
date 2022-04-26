b29661c6396efed1438440863b301d2f3b60979d
SeaSkyThe <marceloer2011@gmail.com>
2022-04-26 15:01:11 -0300

    Iniciando Construção do WebApp

---
+++ src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+#model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    """Pega o filme de entrada e gera as recomendações"""
+    movie = str(request.form["movie"])
+    #prediction = model.predict([[rooms, distance]])
+    #output = round(prediction[0], 2) 
+
+    return render_template('index.html', prediction_text=f'As recomendações para o filme {movie} são: {movie}')
+
+
+if(__name__ == "__main__"):
+    app.run()
